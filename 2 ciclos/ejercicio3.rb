9dba3c6308cda28bb0803ec68ff23352dd1e7a01
danniBr <danitza.br@gmail.com>
2018-06-05 17:25:34 -0400

    commit ciclos ejercicio 3

--- 2 ciclos/ejercicio3.rb
+++ 2 ciclos/ejercicio3.rb
@@ -1,2 +1,21 @@
 # Mostrar todos los divisores del número 990 con:
 # while, for, times.
+
+i = 1
+
+while i <= 990
+	if 990 % i == 0
+		puts i
+	end
+	i += 1	
+end
+
+(1..990).each do |i|
+	puts i if 990 % i == 0
+end
+
+
+for i in 1..990
+	puts i if 990 % i == 0
+end
+
