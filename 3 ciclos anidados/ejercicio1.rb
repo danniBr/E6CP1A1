6383ba6ec7cb2619242fc6d1543c7a982fba0cee
danniBr <danitza.br@gmail.com>
2018-06-16 18:43:06 -0400

    ejercicio 1 ciclos anidados

--- 3 ciclos anidados/ejercicio1.rb
+++ 3 ciclos anidados/ejercicio1.rb
@@ -5,3 +5,18 @@ Se pide imprimir la secuencia numérica, de la siguiente forma:
 3   6   9   12
 4   8   12   16
 =end
+
+
+array = [1,2,3,4]
+
+array.each_with_index do |ele, i|
+	i = "#{ele}"
+
+	print i
+	print i.to_i * 2 
+	print i.to_i * 3 
+	print i.to_i * 4 
+	print "\n"
+
+
+end
